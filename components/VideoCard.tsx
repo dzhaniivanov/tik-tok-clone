5275c885f3d5ef886e38332850c28ffb7828331e
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-07-05 11:36:20 +0300

    start implementing main view component

---
+++ components/VideoCard.tsx
@@ -0,0 +1,12 @@
+import { Video } from "../types";
+import { NextPage } from "next";
+
+interface IProps {
+  post: Video;
+}
+
+const VideoCard: NextPage<IProps> = ({ post }) => {
+  return <div>VideoCard</div>;
+};
+
+export default VideoCard;
