f188e1c82dda2bc336495db745e90992ad47fd11
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-07-05 13:44:47 +0300

    added google identity services logic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -21,6 +23,20 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged in</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => {
+              console.log(response);
+            }}
+            onError={() => console.log("error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
